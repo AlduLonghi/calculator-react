b4d02f3f087d07ee262d6b625b9c77e832124a73
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-01-14 06:40:26 -0300

    Fix linter errors

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <button type="button">
+const Button = ({ name, onClick }) => (
+  <button type="button" onClick={onClick(name)}>
     {name}
   </button>
 );
 
 Button.propTypes = {
   name: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
   name: null,
+  onClick: null,
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,6 +1,7 @@
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-const ButtonPanel = () => {
+const ButtonPanel = props => {
   const buttonNames = [
     ['AC', '+/-', '%', '÷'],
     ['7', '8', '9', 'X'],
@@ -11,23 +12,31 @@ const ButtonPanel = () => {
   return (
     <div>
       <div>
-        {buttonNames[0].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={this.onClick} />) }
+        {buttonNames[0].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={props.onClick} />) }
       </div>
       <div>
-        {buttonNames[1].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={this.onClick} />) }
+        {buttonNames[1].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={props.onClick} />) }
       </div>
       <div>
-        {buttonNames[2].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={this.onClick} />) }
+        {buttonNames[2].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={props.onClick} />) }
       </div>
       <div>
-        {buttonNames[3].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={this.onClick} />) }
+        {buttonNames[3].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={props.onClick} />) }
       </div>
       <div>
-        {buttonNames[4].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={this.onClick} />) }
+        {buttonNames[4].map(btnName => <Button name={btnName} key={`btn-${btnName}`} onClick={props.onClick} />) }
       </div>
     </div>
 
   );
+};
+
+ButtonPanel.propTypes = {
+  onClick: PropTypes.func,
+};
+
+ButtonPanel.defaultProps = {
+  onClick: null,
 };
 
 export default ButtonPanel;
