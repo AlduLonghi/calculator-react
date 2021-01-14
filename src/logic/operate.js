632fbe79cddc119a64a8c131a39cb1b238fc8069
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-01-14 05:25:22 -0300

    Implement calculate and operate methods

---
+++ src/logic/operate.js
@@ -0,0 +1,25 @@
+const operate = (num1, num2, operation) => {
+  let result;
+
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case 'X':
+      result = num1 * num2;
+      break;
+    case '÷':
+      result = num1 / num2;
+      break;
+    case '%':
+      result = (num1 * num2) / 100;
+      break;
+  }
+
+  return result.toString();
+};
+
+export default operate;
