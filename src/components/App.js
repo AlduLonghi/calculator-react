79a4822d5e71e5f52751bfe015873590dd9f5f42
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-01-14 06:10:22 -0300

    Refactor app from stateless function to class

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel onClick={calculate} />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: '',
+      next: '',
+      operation: 'null',
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Display />
+        <ButtonPanel onClick={calculate} />
+      </>
+    );
+  }
+}
 
 export default App;
