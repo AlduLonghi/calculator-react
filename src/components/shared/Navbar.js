59ba2d191e68f497e757d1aa8ca8d3726c030d56
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-01-19 05:01:29 -0300

    Create navbar component

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return(
+        <div>
+            <h1>
+                Math magicians
+            </h1>
+            <div>
+                <link to='/'>Home</link>
+                <link to='/calculator'>Calculator</link>
+                <link to='/quote'>Quote</link>
+            </div>
+        </div>
+    )
+}
