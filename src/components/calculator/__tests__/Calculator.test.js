33ecb6b0a1107a8fef937f66facacc29b143c117
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-01-20 06:00:29 -0300

    Add test for Calculator component

--- src/components/calculator/__tests__/Calculator.test.js
+++ src/components/calculator/__tests__/Calculator.test.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Calculator from '../Calculator';
+
+it('Snapshot test for Calculator component', () => {
+    const tree = renderer
+    .create(<Calculator />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+})
