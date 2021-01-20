d2ec84ebde97d9b4012631b1affc5c8a8ecd80e1
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-01-20 06:12:06 -0300

    Refactor routes and App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import '../style/App.css';
 import Navbar from './shared/Navbar';
 import Footer from './shared/Footer';
 import Routes from '../Routes';
 
 const App = () => (
-  <>
+  <Router>
     <Navbar />
     <div className="page-container">
       <Routes />
     </div>
     <Footer />
-  </>
+  </Router>
 );
 
 export default App;
