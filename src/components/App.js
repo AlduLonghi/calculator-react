f8de7f704fba28f47a88e421c5ac83754947369a
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-01-20 05:04:01 -0300

    Refactor route component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,13 @@
-import { Route, Switch } from 'react-router-dom';
 import '../style/App.css';
 import Navbar from './shared/Navbar';
-import Calculator from './calculator/Calculator';
-import Home from './home/Home';
-import Quote from './quote/Quote';
 import Footer from './shared/Footer';
+import Routes from '../Routes';
 
 const App = () => (
   <>
     <Navbar />
     <div className="page-container">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/calculator" component={Calculator} />
-        <Route path="/quote" component={Quote} />
-      </Switch>
+      <Routes />
     </div>
     <Footer />
   </>
